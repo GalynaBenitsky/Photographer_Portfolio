e4d92c441b858df71048fcd9fa32779f3afcceb8
GalynaM <matsygingalyna@gmail.com>
2021-01-26 12:03:58 -0500

    Phone Screen Layout

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -5,6 +5,12 @@ const Promotions = () => {
   return (
     <div>
       <h1 className="promo">Новини/Акції</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p className="promo">Акції і новини відсутні</p>
     </div>
   );
 };
